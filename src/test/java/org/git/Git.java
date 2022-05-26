73ef8ee957b6886647b2167a515fce559e88b20d
sureshkumarksk <sureshmechanic97@gmail.com>
2022-05-26 19:30:35 +0530

    ocado

---
+++ src/test/java/org/git/Git.java
@@ -0,0 +1,18 @@
+package org.git;
+
+public class Git
+{
+	private void ocadoproduct() {
+		System.out.println("this is the product");
+
+	}
+	private void payment() {
+		System.out.println("this is the payment");
+
+	}
+	private void order() {
+		System.out.println("this is the order");
+
+	}
+
+}
